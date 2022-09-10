035a908d3dca225f5ec2e7310d74ec015eb09580
Антон Дмитриев <highrsalt@gmail.com>
2022-09-10 23:59:16 +0400

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,16 @@ public class Main {
         int current = 200; // текущий баланс счёта
         int added = 1200; // добавлено на счёт
         int bonus;
-        if (added > 1000) { bonus = added / 100;
-        } else { bonus = 0; }
+        if (added > 1000) {
+            bonus = added / 100;
+        } else {
+            bonus = 0;
+        }
 
 
         System.out.println("вы пополнили счёт на " + added);
         System.out.println("размер бонуса составляет " + bonus);
-        System.out.println("текущий баланс счёта: " + (current+added+bonus));
+        System.out.println("текущий баланс счёта: " + (current + added + bonus));
 
-        }
+    }
 }
